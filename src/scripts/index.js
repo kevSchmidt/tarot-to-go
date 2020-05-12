a6e1fc6df66603e3b7ebd1e2384a2c75c79edb12
Kevin <kev.saintigny.schmidt@gmail.com>
2020-05-12 14:16:41 +0200

    base

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,13 +5,39 @@ const cardImage = document.querySelector("#cardImage");
 const cardTitle = document.querySelector("#cardTitle");
 const cardDescription = document.querySelector("#cardDescription");
 
+/* ******
+// LIST CARDS
+const nums = new Set();
+while (nums.size !== 22) {
+  nums.add(Math.floor(Math.random() * 22) + 1);
+}
+let cardsRandom = [];
+nums.forEach((element) => {
+  cardsRandom.push(element);
+});
+console.log(cardsRandom, "this is cardsRandom");
+***** */
+
 // CREATE CARDS SELECTION
+let alreadyUsed = [];
+let result = Math.floor(Math.random() * 22) + 1;
+function myFunction() {
+  alreadyUsed.push(result);
+  if (!alreadyUsed.includes(result)) {
+    return parseInt(result, 10);
+    // return result;
+  }
+  console.log(alreadyUsed);
+  console.log(parseInt(result, 10));
+}
+
 parsedData.forEach((obj) => {
   let { id, backCard } = obj;
   let list = document.querySelector("#cardList");
+
   let cardList = `
-  <li id = ${id} class= "cardSelection"> 
-   <img src = "${backCard}" class = "back-card"></img> 
+  <li id = ${result} class= "cardSelection">
+   <img src = "${backCard}" class = "back-card"></img>
   </li>
   `;
   list.innerHTML += cardList;
@@ -19,7 +45,7 @@ parsedData.forEach((obj) => {
 
 // DISPLAY CARDS
 function display() {
-  let card = parsedData.find((card) => card.id == this.id);
+  let card = parsedData.find((cards) => cards.id == this.id);
   let { name, photo, text } = card;
 
   // Add image
@@ -40,7 +66,6 @@ function display() {
   newDescription.innerHTML = `${text}`;
   cardDescription.appendChild(newDescription);
 }
-
 const listItems = document.querySelectorAll("#cardList li");
 
 // ACTIVE ITEMS
@@ -64,7 +89,9 @@ listItems.forEach((item) => {
   item.addEventListener("click", display);
 });
 
-/*
+document.getElementById("cardButton").addEventListener("click", myFunction);
+
+/* ******************************************************
  // Remove item
  let span = document.createElement("span");
  let text = document.createElement("i");
@@ -78,4 +105,39 @@ listItems.forEach((item) => {
  span.addEventListener("click", () => {
    newItem.remove();
  });
- */
+ ********* */
+
+/* ******************************************************
+// Random cards
+
+const nums = new Set();
+while (nums.size !== 23) {
+  nums.add(Math.floor(Math.random() * 23) + 1);
+}
+
+let cardsRandom = [];
+nums.forEach((element) => {
+  cardsRandom.push(element.toString());
+});
+
+console.log(cardsRandom, "Random numbers pushed");
+
+// Display function
+
+let cardList;
+let displayCards = document.querySelector("#cardList");
+
+let display2 = () => {
+  for (let i = 0; i < 21; i++) {
+    let valuePro = cardsRandom[i];
+
+    let { id, photo } = parsedData[valuePro];
+    cardList = `<div class="card"><img src=${photo}></div>`;
+
+    displayCards.innerHTML += card;
+  }
+  nums = [];
+  cardsRandom = [];
+};
+display2();
+*********************/
